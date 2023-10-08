0bf3ea92f5a302de426801b8d0859a574a77e445
Leul-deg <leuldegarege11@gmail.com>
2023-10-08 15:16:35 +0300

    reset password completed

--- src/resources/auth/auth.routes.ts
+++ src/resources/auth/auth.routes.ts
@@ -3,6 +3,8 @@ import {  signUp, verifyEmail } from './signup'
 import { respond } from '../../utils/respond';
 import { signin } from './signin';
 import { validateRequest } from '../../middlewares/validateRequest';
+import { resetPassword } from './resetPassword';
+import { forgotPassword } from './forgotPassword';
 
 
 const authRouter = express.Router()
@@ -11,7 +13,19 @@ console.log('authRouter');
 authRouter.post('/signup', signUp, respond);
 authRouter.post('/signin', validateRequest, signin, respond);
 authRouter.post('/verify', verifyEmail, respond)
-
-
+authRouter.post(
+    '/forgotPassword',
+  
+    validateRequest,
+    forgotPassword,
+    respond
+  )
+  authRouter.post(
+    '/resetPassword',
+  
+    validateRequest,
+    resetPassword,
+    respond
+  )
 
 export default authRouter
